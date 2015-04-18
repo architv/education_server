ed88561cc30a2b5ef0f1e3e71b8f4f8ad305efe2
Archit Verma <architv07@gmail.com>
2015-04-18 10:20:36 +0000

    adding endpoint for location

--- test1/views.py
+++ test1/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import *
+import requests, json
+
+def get_coordinates(request):
+	postal_code = request.GET.get('postal_code')
+	geometry = requests.get("https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:" + postal_code).json()["results"][0]["geometry"]
+	coordinates = geometry["location"]
+	return HttpResponse(json.dumps(coordinates))
 
 def get_data(request):
 	p = Post.objects.all()
-	return HttpResponse(p) 
+	r = requests.get("https://google.com")
+	return HttpResponse(str(p) + r.text) 
 
 def add_data(request):
 	p = Post()
