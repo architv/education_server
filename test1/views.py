f579a76faa2cb0040f9cbd06d7dbe83140f6c172
Archit Verma <architv07@gmail.com>
2015-04-19 02:49:38 +0000

    improving ivr

--- test1/views.py
+++ test1/views.py
@@ -13,32 +13,22 @@ def call_ivr(request):
 		print event
 		# logger = logging.getLogger('testlogger')
 		# logger.info(event)
-		# if event == "GotDTMF":
-		# 	pincode = int(request.GET['data'])
-		# 	# top_10_schools = SchoolNames.Query.all().limit(10)
-		# 	s = ""
-		# 	for school in top_10_schools:
-		# 		s += school.SCHOOL_NAME
-		# 	r = kookoo.Response()
-		# 	r.addPlayText(s)
-		# 	return HttpResponse(r)
-		# else:
-		r = kookoo.Response()
-		pincode = r.append(kookoo.CollectDtmf(maxDigits=6))
-		top_10_schools = list(SchoolNames.Query.all().limit(20))
-		random_number = randint(1, 20)
-		pincode.append(kookoo.PlayText("Please enter the pincode"))
-		output_string = "The school near you are " + str(top_10_schools[random_number - 1].SCHOOL_NAME)
-		print output_string
-		pincode.addPlayText(".............")
-		pincode.addPlayText(".............")
-		pincode.addPlayText(".............")
-		pincode.addPlayText(".............")
-		pincode.addPlayText(".............")
-		pincode.addPlayText(".............")
-		pincode.addPlayText(output_string)
-		r.addHangup()
-		return HttpResponse(r)
+		if event == "GotDTMF":
+			pincode = int(request.GET['data'])
+			top_10_schools = SchoolNames.Query.all().limit(10)
+			s = ""
+			for school in top_10_schools:
+				s += school.SCHOOL_NAME
+			r = kookoo.Response()
+			r.addPlayText(s)
+			return HttpResponse(r)
+		else:
+			r = kookoo.Response()
+			pincode = r.append(kookoo.CollectDtmf(maxDigits=6))
+			# top_10_schools = list(SchoolNames.Query.all().limit(20))
+			# random_number = randint(1, 20)
+			pincode.append(kookoo.PlayText("Please enter the pincode"))
+			return HttpResponse(r)
 
 def get_coordinates(request):
 	postal_code = request.GET.get('postal_code')
