7e1adc1bf8c1ee72afef531c0a664284496a92f2
Archit Verma <architv07@gmail.com>
2015-04-19 07:44:28 +0000

    improving ivr

--- test1/views.py
+++ test1/views.py
@@ -26,7 +26,7 @@ def call_ivr(request):
 		else:
 			r = kookoo.Response()
 			pincode = r.append(kookoo.CollectDtmf(maxDigits=6))
-			pincode.append(kookoo.PlayAudio("pincode.amr"))
+			pincode.append(kookoo.PlayAudio("pincode.mp3"))
 			# pincode.append(kookoo.PlayText("Please enter the pincode"))
 			return HttpResponse(r)
 
