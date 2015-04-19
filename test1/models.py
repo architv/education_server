1c422dc55dbdd50dacc812cda23318dc3fc363df
Archit Verma <architv07@gmail.com>
2015-04-19 01:46:53 +0000

    logging heroku

--- test1/models.py
+++ test1/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from parse_rest.connection import register
+from parse_rest.datatypes import Object, GeoPoint
+from parse_rest.user import User
+import json, requests
+
+register('P3N8sjOcUB7pMF3G1miWIfswdXGx6MN2MzBJ8I3M', 'EGxO4lNrnlbYcDz6h36vAFLDTMiLG4tSVyU2RRE0')
 
 # Create your models here.
 class Post(models.Model):
@@ -8,3 +14,6 @@ class Post(models.Model):
 
 	def __unicode__(self):
 		return self.title
+
+class SchoolNames(Object):
+	pass
--- test1/views.py
+++ test1/views.py
@@ -4,11 +4,30 @@ from .models import *
 import requests, json
 import xml.etree.ElementTree as ET
 import kookoo
+import logging
 
 def call_ivr(request):
-	r = kookoo.Response()
-	r.addPlayText("Laavanye I love you")
-	return HttpResponse(r)
+	if request.method == 'GET':
+		print request.GET
+		event = request.GET.get('event', None)
+		print event
+		logger = logging.getLogger('testlogger')
+		logger.info(event)
+		if event == "GotDTMF":
+			pincode = int(request.GET['data'])
+			top_10_schools = SchoolNames.Query.all().limit(10)
+			s = ""
+			for school in top_10_schools:
+				s += school.SCHOOL_NAME
+			r = kookoo.Response()
+			r.addPlayText(s)
+			return HttpResponse(r)
+		else:
+			r = kookoo.Response()
+			pincode = r.append(kookoo.CollectDtmf(maxDigits=6))
+			pincode.append(kookoo.PlayText("Please enter the input"))
+			return HttpResponse(r)
+		return HttpResponse("error")
 
 def get_coordinates(request):
 	postal_code = request.GET.get('postal_code')
